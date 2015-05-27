cd1183d1a670c7ff9ef39030ec5a7ab1e40f5dc2
ritz078 <rkritesh078@gmail.com>
2015-05-27 19:15:13 +0530

    saving feature added

--- matlab.m
+++ matlab.m
@@ -3,6 +3,7 @@ y=0;
 while y >= 0
     c=clock;
     c=fix(c);
+    x.clock=c;
     x.hours=c(:,4);
     x.minutes=c(:,5);
     x.seconds=c(:,6);
--- server.js
+++ server.js
@@ -3,30 +3,85 @@ var mongoose = require('mongoose');
 var fs = require('fs');
 var app = express();
 
-// mongoose.connect('mongodb://localhost/matlabMeanDemo');
+mongoose.connect('mongodb://localhost/matlabMeanDemo');
 
 var io = require('socket.io')(app.listen(3000));
 
 
-/*==========  Receive the request and send response  ==========*/
 app.use(express.static(__dirname + '/app'));
 
-io.on('connection', function(socket) {
-    fs.watch('data', function(event, filename) {
-        fs.readFile('data/' + filename, function(err, data) {
-            if (!err) {
-                try {
-                    var x = JSON.parse(data);
-                    socket.emit('updated', x);
-                } catch (e) {
-                    console.log('malformed data');
-                }
+var appData;
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', function callback() {
+
+	/*============================================================
+	=            Creating the schema to save the data            =
+	============================================================*/
+	var dataSchema;
+
+    dataSchema = mongoose.Schema({
+        clock: Array,
+        hours: Number,
+        minutes: Number,
+        seconds: Number
+    });
+
+    appData = mongoose.model('appData', dataSchema);
+	
+	
+	/*-----  End of Creating the schema to save the data  ------*/
+
+
+
+    //Sending and receiving data
+    io.on('connection', function(socket) {
+        fs.watch('data', function(event, filename) {
+            fs.readFile('data/' + filename, function(err, data) {
+                if (!err) {
+                    try {
+                        var x = JSON.parse(data);
+                        socket.emit('updated', x);
+
+                        /*==================================================
+                        =            Saving the data in MongoDB            =
+                        ==================================================*/
+
+                        var savingData = new appData({
+                            clock: x.clock,
+                            hours: x.hours,
+                            minutes: x.minutes,
+                            seconds: x.seconds
+                        });
+
+                        savingData.save(function(error, savedData) {
+                            if (error) {
+                                console.log("error in saving");
+                            }
+                            console.log(savedData);
+                        });
+                        
+                        
+                        /*-----  End of Saving the data in MongoDB  ------*/
+                        
                     
-            
-        }
-    })
+
+                    } catch (e) {
+                        console.log('malformed data');
+                    }
+
+
+                }
+            })
+        });
+    });
+
+
 });
-});
+
 
 app.get("/", function(req, res) {
     res.sendfile("/index.html");
